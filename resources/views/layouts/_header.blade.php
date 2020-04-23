8e64d95156c4a1a2229765d28bf28d2ff14320ff
hcg <673035915@qq.com>
2020-04-23 11:11:16 +0000

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container ">
-    <a class="navbar-brand" href="/">福大校友录</a>
+    <a class="navbar-brand" href="{{route('home')}}">福大校友录</a>
     <ul class="navbar-nav justify-content-end">
-      <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+      <li class="nav-item"><a class="nav-link" href="{{route('help')}}">帮助</a></li>
       <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
     </ul>
   </div>
