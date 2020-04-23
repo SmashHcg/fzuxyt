fed768fc79bc7157d157e5ceca2cdb110545bd0e
hcg <673035915@qq.com>
2020-04-23 10:35:53 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', '福大校友录') - 致力于更好的校友服务</title>
+    <title>@yield('title', '福大校友录') - 随时随地看望母校</title>
+    <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @yield('content')
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" href="/">福大校友录</a>
+        <ul class="navbar-nav justify-content-end">
+          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,15 @@
 @extends('layouts.default')
+
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>欢迎!</h1>
+    <p class="lead">
+      你现在所看到的是由本人制作的 <a href="https://learnku.com/courses/laravel-essential-training">福大校友录</a> 的毕业设计项目主页。
+    </p>
+    <p>
+      Everything of my project will start showing soon.
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+  </div>
 @stop
