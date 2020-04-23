79620627229685fe4d8e46ab6c9d422a7a02c61e
hcg <673035915@qq.com>
2020-04-23 15:12:58 +0000

    用户注册表单

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,5 +2,41 @@
 @section('title', '注册')
 
 @section('content')
-<h1>注册</h1>
+<div class="offset-md-2 col-md-8">
+  <div class="card ">
+    <div class="card-header">
+      <h5>注册</h5>
+    </div>
+    <div class="card-body">
+      <form method="POST" action="{{ route('users.store') }}">
+          <div class="form-group">
+            <label for="account">账号：</label>
+            <input type="text" name="account" class="form-control" value="{{ old('account') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="email">邮箱：</label>
+            <input type="text" name="email" class="form-control" value="{{ old('email') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="nickName">姓名：</label>
+            <input type="text" name="nickName" class="form-control" value="{{ old('nickName') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password">密码：</label>
+            <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+          </div>
+
+          <div class="form-group">
+            <label for="password_confirmation">确认密码：</label>
+            <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
+          </div>
+
+          <button type="submit" class="btn btn-primary">注册</button>
+      </form>
+    </div>
+  </div>
+</div>
 @stop
